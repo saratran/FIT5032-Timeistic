17e33f2905f55628240af0033a68b8b5b0bb5826
Uyen Tran <utra0001@student.monash.edu>
2020-10-24 16:31:08 +1100

    Add rich text editor for email content

--- FIT5032Project/Models/SendEmailViewModel.cs
+++ FIT5032Project/Models/SendEmailViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace FIT5032Project.Models
 {
@@ -17,6 +18,7 @@ namespace FIT5032Project.Models
         public string Subject { get; set; }
 
         [Required(ErrorMessage = "Please enter the contents")]
+        [AllowHtml]
         public string Contents { get; set; }
     }
 }
