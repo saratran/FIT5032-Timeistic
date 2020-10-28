9412a5377e2059c6f90e102cff4708f35ad3abce
Uyen Tran <utra0001@student.monash.edu>
2020-10-28 21:31:44 +1100

    Simple chart for task

---
+++ FIT5032Project/Controllers/ReportController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032Project.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FIT5032Project.Controllers
+{
+    [Authorize]
+    public class ReportController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        protected UserManager<ApplicationUser> UserManager { get; set; }
+
+        public ReportController()
+        {
+            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(this.db));
+        }
+
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var items = db.Items.Where(i => i.User.Id == userId).OrderByDescending(item => item.Date).ThenBy(item => item.StartTime).ToList();
+            return View(items);
+        }
+
+
+    }
+}
---
+++ FIT5032Project/Models/Report.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032Project.Models
+{
+    public class Report
+    {
+        IList<Item> items { get; set; }
+
+        public Report(IList<Item> items)
+        {
+            this.items = items;
+        }
+    }
+}
